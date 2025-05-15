1245782c196fcec2e9d725ed92e4083959c30cb8
davidkonrad <davidkonrad@gmail.com>
2025-05-15 21:04:45 +0200

    fix save settings

--- src/playground.js
+++ src/playground.js
@@ -1,7 +1,6 @@
 /*
  * Playground for suneditor-emojis
  * Copyright 2025- David Konrad
- *
  */
 
 "use strict";
@@ -11,6 +10,7 @@ const Playground = (function() {
 	const qsel = (sel) => { return document.querySelector(sel) }
 	const qall = (sel) => { return document.querySelectorAll(sel) }
 	const default_iconSize = '1.7rem'
+	const storage_prefix = 'suneditor-emoji_'
 	let editor = undefined
 	let version = undefined
 	let initmode = undefined
@@ -18,6 +18,8 @@ const Playground = (function() {
 	const init = function() {
 		fetchVersions()
 		initForm()
+		initStorage()
+		initFooter()
 	}
 
 	const wait = function(mode) {
@@ -57,7 +59,6 @@ const Playground = (function() {
 				})
 			} catch(error) {
 				console.log('fetchVersions', error)
-				resolve(error)
 			}
 		}
 	}
@@ -74,7 +75,7 @@ const Playground = (function() {
 			}
 		})
 		setTimeout(function() {
-			sel.insertAdjacentHTML('afterbegin', '<option value="latest" style="color:gray;" title="v3 is not supported (yet)">latest</option>')
+			sel.insertAdjacentHTML('afterbegin', '<option value="latest" title="@latest is 2.47.5">latest</option>')
 			sel.value = version
 		})
 		sel.onchange = function() {
@@ -110,7 +111,7 @@ const Playground = (function() {
 	}
 
 	const storage = function(o) {
-		const name = (s) => 'suneditor-emoji_' + s
+		const name = (s) => storage_prefix + s
 		if (typeof o === 'string') {
 			return localStorage.getItem(name(o))
 		}
@@ -122,6 +123,19 @@ const Playground = (function() {
 		}
 	}
 
+	const initFooter = function() {
+		gebi('footer-reset-storage').onclick = function() {
+			Object.keys(localStorage).forEach(function(key){
+				if (key.startsWith(storage_prefix)) {
+					localStorage.removeItem(key)
+				}
+			})
+			setTimeout(function() {
+				location.reload()
+			})
+		}
+	}
+			
 	const initOrderable = function(cnt, name) {
 		const items = cnt.querySelectorAll('.item-sortable')
 		let dragged = undefined
@@ -207,15 +221,32 @@ const Playground = (function() {
 	}
 
 	const initForm = function() {
-		const groups = ['Smileys & Emotion', 'Activities', 'Animals & Nature', 'Flags', 'Food & Drink', 'Objects', 'People & Body', 'Symbols', 'Travel & Places']
+		let gc = storage('save-groups-captions') || false
+		const check_storage = storage('save-settings') || true
 		const div = gebi('option-groups-captions').querySelector('div')
-		for (const group of groups) {
-			div.insertAdjacentHTML('beforeend', `<div class="item-sortable" style="display:table-row;" title="${group}">
+		if (gc && check_storage !== 'false') {
+			gc = JSON.parse(gc)
+			gc.forEach(function(item) {
+				const c = item.checked ? 'checked' : ''
+				const d = item.checked ? '' : 'disabled'
+				const s = item.checked ? 'display:table-row;' : 'display:table-row;color:rgb(170, 170, 170);'
+				div.insertAdjacentHTML('beforeend', `<div class="item-sortable" style="${s}" title="${item.group}">
+					<label class="fieldset-item" style="display:table-cell;cursor:pointer">
+						<input type="checkbox" name="${item.group}" ${c}>${item.group}</label>
+					</label>
+					<input class="fieldset-item" type="text" name="${item.group}" value="${item.caption}" style="display:table-cell;" spellcheck="false" ${d}>
+				</div>`)
+			})
+		} else {
+			const groups = ['Smileys & Emotion', 'Activities', 'Animals & Nature', 'Flags', 'Food & Drink', 'Objects', 'People & Body', 'Symbols', 'Travel & Places']
+			for (const group of groups) {
+				div.insertAdjacentHTML('beforeend', `<div class="item-sortable" style="display:table-row;" title="${group}">
 					<label class="fieldset-item" style="display:table-cell;cursor:pointer">
 						<input type="checkbox" name="${group}" checked>${group}</label>
 					</label>
 					<input class="fieldset-item" type="text" name="${group}" value="${group}" style="display:table-cell;" spellcheck="false">
 				</div>`)
+			}
 		}
 
 		div.insertAdjacentHTML('beforeend', `<div style="display:table-row;font-size:small;user-select:false;">
@@ -228,16 +259,12 @@ const Playground = (function() {
 		initOrderable(div, 'groups-captions')
 		initGroups()
 
-		qsel('#option-topmenu div').querySelectorAll('input[type="checkbox"]').forEach(function(input) {
-			input.onclick = updateEditor
-		})
 		initOrderable(qsel('#option-topmenu div'), 'topmenu')
-
-		qsel('#option-showRecent input').onclick = updateEditor
 
 		const updateCurrent = function(current) {
 			gebi('iconsize-current').innerText = '(' +  current +')'
 		}
+
 		const select = gebi('select-iconsize')
 		let size = 0.3
 		while (size < 5) {
@@ -247,26 +274,13 @@ const Playground = (function() {
 			option.style.fontSize = fs
 			option.innerText = '😊'
 			option.title = fs
-			if (fs === default_iconSize) {
-				option.selected = true
-				updateCurrent(fs)
-			}			
 			select.appendChild(option)
 			size += 0.2
 		}
-		select.onchange = function() {
+		updateCurrent(storage('save-iconSize') || default_iconSize)
+		select.addEventListener('change', function() {
 			updateCurrent(this.value)
-			updateEditor()
-		}		
-
-		qall('#option-skinTone input[name="skinTone"]').forEach(function(input) {
-			input.onclick = updateEditor
 		})
-
-		qsel('#option-showFallbacks input').onclick =	updateEditor
-		qsel('#option-tagName select').onchange = updateEditor
-		qsel('#option-width select').onchange = updateEditor
-		qsel('#option-height select').onchange = updateEditor
 
 		qall('aside details').forEach(function(d) {
 			d.ontoggle = function() {
@@ -276,6 +290,108 @@ const Playground = (function() {
 		})
 	}
 
+	const initStorage = function() {
+		const check_storage = gebi('check-save-settings')
+		check_storage.onclick = function() {
+			storage({'save-settings': this.checked.toString() })
+		}
+		const ss = storage('save-settings')
+		if (ss === 'true') {
+			check_storage.checked = true
+		} else if (ss === 'false') {
+			check_storage.checked = false
+		} else {
+			check_storage.checked = true
+		}
+
+		const initCheck = function(sel, storName, defVal) {
+			const val = storage(storName)
+			const input = qsel(sel)
+			if (check_storage.checked) {
+				if (val === 'true') {
+					input.checked = true
+				} else if (val === 'false') {
+					input.checked = false
+				} else {
+					input.checked = defVal
+				}
+			}
+			input.addEventListener('click', function() {
+				updateEditor()
+				if (check_storage.checked) {
+					storage({ [storName]: this.checked.toString() })
+				}
+			})
+		}
+
+		const initSelect = function(sel, storName, defVal) {
+			const val = storage(storName)
+			const select = qsel(sel)
+			if (check_storage.checked) {
+				select.value = val ? val : defVal
+			}
+			select.addEventListener('change', function() {
+				updateEditor()
+				if (check_storage.checked) {
+					storage({ [storName]: this.value })
+				}
+			})
+		}
+
+		const skinTone = storage('save-skinTone') || 'neutral'
+		qall('#option-skinTone input[name="skinTone"]').forEach(function(input) {
+			if (check_storage.checked) if (input.value === skinTone) input.checked = true
+			input.addEventListener('click', function() {
+				updateEditor()
+				if (check_storage.checked) {
+					storage({ 'save-skinTone': this.value })
+				}
+			})
+		})
+
+		initCheck('#option-showFallbacks input', 'save-showFallbacks', true)
+		initCheck('#option-showRecent input', 'save-showRecent', false)
+		initCheck('#option-topmenu input[name="search"]', 'save-topMenu-search', false)
+		initCheck('#option-topmenu input[name="iconSize"]', 'save-topMenu-iconSize', false)
+		initCheck('#option-topmenu input[name="skinTone"]', 'save-topMenu-skinTone', false)
+
+		initSelect('#option-tagName select', 'save-tagName', 'span')
+		initSelect('#option-width select', 'save-width', '')
+		initSelect('#option-height select', 'save-height', '')
+		initSelect('#option-iconSize select', 'save-iconSize', default_iconSize)
+
+		initCheck('#option-captions-disable', 'save-captions-disable', false)
+		const cd = storage('save-captions-disable')
+		if (cd === 'true') {
+			qall('#option-groups-captions input[type="text"]').forEach(function(input) {
+				input.setAttribute('disabled', 'disabled')
+			})
+		}
+
+		//groups and captions
+		const updateStorage = function() {
+			const items = []	
+			qall('#option-groups-captions .item-sortable').forEach(function(item) {
+				items.push({
+					group: item.querySelector('input[type="checkbox"]').getAttribute('name'),
+					checked: item.querySelector('input[type="checkbox"]').checked,
+					caption: item.querySelector('input[type="text"]').value
+				})
+			})
+			storage({ 'save-groups-captions': items })
+		}
+		qall('#option-groups-captions input[type="text"]').forEach(function(input) {
+			input.addEventListener('input', function() {
+				updateStorage()
+			})
+		})
+		qall('#option-groups-captions .item-sortable').forEach(function(item) {
+			item.addEventListener('dragend', function() {
+				updateStorage()
+			})
+		})
+	}
+	
 	const updateOptions = function(options) {
 		let s = ''
 		if (options) {
@@ -355,11 +471,12 @@ const Playground = (function() {
 		opt = opt || {}
 		//const options = {}
 		let options = undefined
+		const focus = editor ? false : true
 		if (editor) editor.destroy()
 		switch (initmode) {
 			case 'v3' :
 				options = {
-					plugins: SUNEDITOR.Plugins,
+					plugins: SUNEDITOR.Plugins, // eslint-disable-line no-undef
 					buttonList: [
 						['font', 'fontSize', 'formatBlock'], // ['emojis'],
 						['bold', 'underline', 'italic', 'strike', 'removeFormat'],
@@ -388,7 +505,7 @@ const Playground = (function() {
 				break;
 		}
 		qsel('.sun-editor-editable').tabIndex = -1
-		qsel('.sun-editor-editable').focus()
+		if (focus) qsel('.sun-editor-editable').focus()
 	}
 
 	return {
